98abbb4106e00bf09b43b531bfa50a8906ca2d87
Tetsuro Mikami <tetsuro@mickamy.com>
2025-11-12 17:44:05 +0900

    docker: create events/snapshots table on init container

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,1 +1,24 @@
 CREATE DATABASE ges;
+
+\c ges
+
+CREATE TABLE IF NOT EXISTS events
+(
+    stream_id  TEXT        NOT NULL,
+    version    BIGINT      NOT NULL,
+    event_id   UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    event_type TEXT        NOT NULL,
+    payload    JSONB       NOT NULL,
+    metadata   JSONB       NOT NULL DEFAULT '{}'::jsonb,
+    at         TIMESTAMPTZ NOT NULL DEFAULT now(),
+    PRIMARY KEY (stream_id, version),
+    UNIQUE (event_id)
+);
+
+CREATE TABLE IF NOT EXISTS snapshots
+(
+    stream_id TEXT PRIMARY KEY,
+    version   BIGINT      NOT NULL,
+    state     JSONB       NOT NULL,
+    at        TIMESTAMPTZ NOT NULL DEFAULT now()
+);
